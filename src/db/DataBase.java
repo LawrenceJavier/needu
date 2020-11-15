be0a364db4f451211eed9634e4e5c47bcc84e847
lawrence <ljavierminguillan@gmail.com>
2020-11-15 20:04:16 +0100

    update
    Signed-off-by: lawrence <ljavierminguillan@gmail.com>

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -240,8 +240,6 @@ public class DataBase {
     *@param edad edad del paciente
     *@param dni dni del paciente
     *@param peso peso del pacientes
-    //hola
-    //adios
     */
 
     public void actualizarPrivado(String correo, String dni, String direccion, String sexo, String telefono) {
